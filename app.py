dd8692c2eddff3da7ecf7f5749114ac9f52bd68b
Noizeboi <155340506+Noizeboi@users.noreply.github.com>
2025-07-09 00:40:57 +0100

    ..

--- app.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-from logic import suggest_maintenance
-from data import dropdown_models
-
--- data.py
+++
@@ -1,7 +0,0 @@
-import json
-
-try:
-    with open("dropdown_models.json", "r") as f:
-        dropdown_models = json.load(f)
-except FileNotFoundError:
-    dropdown_models = {}
